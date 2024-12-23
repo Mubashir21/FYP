938a0c518188b6c8fdf5d31288ad433be23cea98
Mubashir21 <mubashirshoukat@gmail.com>
2024-12-23 19:08:18 +0800

    made the frontend dir. Initialised the nextJS app and did a few setup changes.

---
+++ frontend/wildtechalert/components/header.tsx
@@ -0,0 +1,37 @@
+import {
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
+
+export default function Header() {
+  return (
+    <header>
+      <div className="flex justify-around items-center">
+        <p>WildTechAlert</p>
+        <div>
+          {/* <Menubar>
+            <MenubarMenu>
+              <MenubarTrigger>File</MenubarTrigger>
+              <MenubarContent>
+                <MenubarItem>
+                  New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+                </MenubarItem>
+                <MenubarItem>New Window</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Share</MenubarItem>
+                <MenubarSeparator />
+                <MenubarItem>Print</MenubarItem>
+              </MenubarContent>
+            </MenubarMenu>
+          </Menubar> */}
+          Hello
+        </div>
+      </div>
+    </header>
+  );
+}
