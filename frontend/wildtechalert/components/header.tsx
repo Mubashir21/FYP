9d5f774ac6977e4aeae22e3084b4aa2d01df18c8
Mubashir21 <mubashirshoukat@gmail.com>
2025-05-01 18:59:39 +0800

    changed the hero bg on user page.

--- frontend/wildtechalert/components/header.tsx
+++ frontend/wildtechalert/components/header.tsx
@@ -1,7 +1,15 @@
 import Logo from "../public/logo.svg";
 import Image from "next/image";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "./ui/navigation-menu";
+import Link from "next/link";
+import { Button } from "./ui/button";
 
-const sections = [
+const navItems = [
   { name: "Home", link: "/home" },
   { name: "About Us", link: "/aboutus" },
   { name: "System Features", link: "/features" },
@@ -12,16 +20,26 @@ const sections = [
 
 export default function Header() {
   return (
+    // <header className="bg-black text-white">
     <header>
-      <div className="flex justify-between items-center py-5 mx-80">
+      <div className="flex justify-between items-center mx-80">
         <Image src={Logo} alt="Logo" width={50} height={50} />
-        <div className="flex gap-5">
-          {sections.map(({ name, link }) => (
-            <a key={name} href={link} className="hover:underline">
-              {name}
-            </a>
-          ))}
-        </div>
+        <NavigationMenu>
+          <NavigationMenuList>
+            {navItems.map((item) => (
+              <NavigationMenuItem key={item.name}>
+                <Link href={item.link} legacyBehavior passHref>
+                  <NavigationMenuLink className="px-2 ">
+                    {item.name}
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            ))}
+          </NavigationMenuList>
+        </NavigationMenu>
+        <Button variant="outline">
+          <Link href="/login">Login</Link>
+        </Button>
       </div>
     </header>
   );
